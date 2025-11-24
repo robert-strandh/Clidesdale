5cd4aa92abbd5f39defd297190de7dedce0a4fcc
Robert Strandh <robert.strandh@gmail.com>
2025-11-24 15:29:12 +0100

    Define class WARNING.

--- Code/history.lisp
+++ Code/history.lisp
@@ -1,8 +1,12 @@
 (cl:in-package #:clidesdale)
 
+(defclass warning ()
+  ((%condition :initarg :condition :reader condition)
+   (%location :initarg :location :initform nil :reader location)))
+
 (defclass history-item ()
   ((%text :initarg :text :reader text)
-   (%current-package :initarg :current-package :reader current-package)
+   (%package :initarg :package :reader package)
    (%warnings :initarg :warnings :initform '() :reader warnings)))
 
 (defclass history ()
