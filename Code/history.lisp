a9ba8d2657e64575669a1004e9d01ae90a6a8f29
Robert Strandh <robert.strandh@gmail.com>
2025-11-24 15:45:22 +0100

    Supply explicit DEFGENERIC forms for slot accessors.

--- Code/history.lisp
+++ Code/history.lisp
@@ -1,13 +1,25 @@
 (cl:in-package #:clidesdale)
 
+(defgeneric condition (warning))
+
+(defgeneric location (warning))
+
 (defclass warning ()
   ((%condition :initarg :condition :reader condition)
    (%location :initarg :location :initform nil :reader location)))
+
+(defgeneric text (history-item))
+
+(defgeneric package (history-item))
+
+(defgeneric warnings (history-item))
 
 (defclass history-item ()
   ((%text :initarg :text :reader text)
    (%package :initarg :package :reader package)
    (%warnings :initarg :warnings :initform '() :reader warnings)))
+
+(defgeneric items (history))
 
 (defclass history ()
   ((%items :initarg :items :accessor items)))
