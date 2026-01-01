9fec48fc636252a20e440a6e34d6cf3742e22c64
Robert Strandh <robert.strandh@gmail.com>
2026-01-01 07:39:43 +0100

    Add top level to control prompt.

--- Code/Listener/application-frame.lisp
+++ Code/Listener/application-frame.lisp
@@ -3,11 +3,14 @@
 (clim:define-application-frame listener ()
   ()
   (:panes (interactor :interactor))
+  (:top-level (clim:default-frame-top-level
+               :prompt (format nil "Form:~%")))
   (:layouts (default
              (clim:vertically ()
                (clim:scrolling (:scroll-bars t)
                  interactor)))))
 
 (defun listener ()
-  (let ((frame (clim:make-application-frame 'listener)))
+  (let ((frame (clim:make-application-frame 'listener
+                 :width 1000 :height 1000)))
     (clim:run-frame-top-level frame)))
